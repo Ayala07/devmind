2fa2f89d64b69a23f84039293e6767ec2c2f4cc1
Roberto <robertube26@gmail.com>
2021-11-02 18:00:32 -0600

    feat: project configured

---
+++ components/ui/input/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <VsInput label-placeholder="TEXTO"/>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'UiInput',
+  setup() {
+
+  },
+})
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="m-5">
+    <UiInput />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
